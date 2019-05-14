62c55d97c0646e7fcdf168a8d69ec3d59cf8bf52
Jon Booraem <jbooraem@ctdi.com>
2019-05-14 12:07:50 -0500

    Added class for OLP results

---
+++ Verifone_MX9/OLPResults.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Verifone_MX9
+{
+    class OLPResults
+    {
+        public OLPResults()
+        {
+            Results = new List<TestResult>();
+        }
+        public bool IsPassed { get; set; }
+        public string tamperCode { get; set; }
+        public IList<TestResult> Results { get; set; }
+    }
+
+    class TestResult
+    {
+        public string Code { get; set; }
+        public bool IsPassed { get; set; }
+    }
+}
